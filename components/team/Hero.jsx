f127a8e2e6cfd4be4ac4b1fb13c8910ada4c041d
Nweke Chidi <nwekechidigodwin460@gmail.com>
2023-05-03 22:32:30 +0100

    add team page desktop
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- components/team/Hero.jsx
+++ components/team/Hero.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import Abayomi from "../../images/abayomi.jpg";
+import Daniel from "../../images/daniel.jpg";
+import Belen from "../../images/belen.jpg";
+import Oscar from "../../images/oscar.jpg";
+import Vector2 from "../../images/Vector2.png";
 
 function Hero() {
   return (
     <section className="pt-[106px] pb-10 font-raleway">
       <div className="max-w-screen-2xl m-auto px-6 sm:px-12 md:px-24 w-full">
-        <div className="flex flex-row justify-between ">
-          <div>
-            <h3 className=" font-bold text-xl  lg:text-[32px] text-black text-left ">
+        <div className="flex flex-col lg:flex-row gap-32 mt-16 lg:mt-32">
+          <div className="w-4/12">
+            <h3 className=" font-bold text-xl  lg:text-4xl text-black text-left mb-6">
               Meet our Team of Experts
             </h3>
-            <p className="font-medium text-sm lg:text-xl text-black text-left">
+            <p className="font-medium text-sm lg:text-xl text-black text-left mb-4">
               At Sixteen Sands, we&apos;re proud to have a team of experts who
               are passionate about revolutionising the agriculture industry. Our
               team includes engineers, data scientists, and agricultural
@@ -27,7 +33,44 @@ function Hero() {
               agriculture.
             </p>
           </div>
-          <div>Images or something</div>
+          <div className="grid grid-cols-2 gap-x-20 gap-y-10">
+            <div className="">
+                <Image src={Abayomi} alt="Abayomi Awobokun Picture" width={205} height={199} className="max-w-[205px]"/>
+                <h5 className="mt-5 font-bold text-xl">Abayomi Awobokun</h5>
+                <div className=" flex flex-row justify-between items-center w-full mt-2">
+                    <p className="italic text-sm ">Director</p>
+                    <Image src={Vector2} alt="Short Arrow" width={44} height={8} className="h-2"/>
+                </div>
+            </div>
+           
+            <div className="">
+                <Image src={Daniel} alt="Daniel L Ayuba Picture" width={205} height={199} className="max-w-[205px]"/>
+                <h5 className="mt-5 font-bold text-xl">Daniel L Ayuba</h5>
+                <div className=" flex flex-row justify-between items-center w-full mt-2">
+                    <p className="italic text-sm ">Director</p>
+                    <Image src={Vector2} alt="Short Arrow" width={44} height={8} className="h-2"/>
+                </div>
+            </div>
+
+            <div className="">
+                <Image src={Oscar} alt="Dr Oscar Mendez Picture" width={205} height={199} className="max-w-[205px]"/>
+                <h5 className="mt-5 font-bold text-xl">Dr Oscar Mendez</h5>
+                <div className=" flex flex-row justify-between items-center w-full mt-2">
+                    <p className="italic text-sm ">Director</p>
+                    <Image src={Vector2} alt="Short Arrow" width={44} height={8} className="h-2"/>
+                </div>
+            </div>
+
+
+            <div className="">
+                <Image src={Belen} alt="Belen Marti-Cardona Picture" width={205} height={199} className="max-w-[205px]"/>
+                <h5 className="mt-5 font-bold text-xl">Belen Marti-Cardona</h5>
+                <div className=" flex flex-row justify-between items-center w-full mt-2">
+                    <p className="italic text-sm ">Director</p>
+                    <Image src={Vector2} alt="Short Arrow" width={44} height={8} className="h-2"/>
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
