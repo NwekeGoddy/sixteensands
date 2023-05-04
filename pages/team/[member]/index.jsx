7d62281a589cb1c470dbc0d4e4381238e436a1dd
Nweke Chidi <nwekechidigodwin460@gmail.com>
2023-05-04 19:17:38 +0100

    update again
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- pages/team/[member]/index.jsx
+++ pages/team/[member]/index.jsx
@@ -6,10 +6,6 @@ import Header from "@/components/layout/Header";
 import MeetTeamIntro from "@/components/team/MeetTeamIntro";
 import { MemberData } from "../../../Data/memberdata";
 
-import Abayomi from "../../../images/abayomi.jpg";
-import Daniel from "../../../images/daniel.jpg";
-import Belen from "../../../images/belen.jpg";
-import Oscar from "../../../images/oscar.jpg";
 import Vector2 from "../../../images/Vector2.png";
 
 function EachMember() {
@@ -22,7 +18,7 @@ function EachMember() {
       <section className="pt-[106px] pb-10 font-raleway">
         <div className="max-w-screen-2xl m-auto px-6 sm:px-12 md:px-24 w-full">
           <div className="flex flex-col lg:flex-row gap-16 mt-16 lg:mt-32">
-            <div className="hidden md:block">
+            <div >
               <MeetTeamIntro />
             </div>
             <div className="w-full md:w-8/12">
